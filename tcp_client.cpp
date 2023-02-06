11cd1b00f4cbdd3c5c9610400626091f22a29d7e
Greg Sarandi <sarandi@usc.edu>
2023-02-06 01:39:10 -0800

    Update tcp_client.cpp
    Updated client program in C++ using sockets tutorial from lab2 report pdf

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -16,13 +16,14 @@ int main(int argc, char const *argv[])
 	char socket_read_buffer[1024];
 	
 	// TODO: Fill out the server ip and port
-	std::string server_ip = "";
-	std::string server_port = "";
+	std::string server_ip = "127.20.10.4"; //IP address of raspberry pi
+	std::string server_port = "12345"; //specific port number to enter in server
 
 	int opt = 1;
 	int client_fd = -1;
 
 	// TODO: Create a TCP socket()
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);
 
 	// Enable reusing address and port
 	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
@@ -44,10 +45,32 @@ int main(int argc, char const *argv[])
 	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
 
 	// TODO: Connect() to the server (hint: you'll need to use server_addr)
+	if (connect(client_fd, server_addr->ai_addr, server_addr->ai_addrlen) < 0) {
+		
+		printf("Error connecting to server\n");
+
+	}
+
 	// TODO: Retreive user input
+
+	printf("Please enter message: ");
+    bzero(socket_read_buffer,1024); //erases the data in the 1024 bytes of socket_read_buffer, rewriting them as zeroes
+    fgets(socket_read_buffer,1023,stdin); //reads input from client and stores it in socket_read_buffer
+
 	// TODO: Send() the user input to the server
+
+	send(client_fd, socket_read_buffer, 1024, 0);
+
+
 	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
+
+	bzero(socket_read_buffer, 1024); //erases the data in socket_read_buffer, rewriting it as zeroes
+    read(client_fd, socket_read_buffer, 1024); 
+    printf("Message sent from server: %s\n", socket_read_buffer);
+
 	// TODO: Close() the socket
+
+	close(client_fd);
 
 	return 0; 
 } 
